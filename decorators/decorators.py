38a35db036b956260b912c61e50ae6280959704c
Georgi Kukov <gmkukov@gmail.com>
2020-04-06 20:07:58 +0300

    Solution to accepts decorator

---
+++ decorators/decorators.py
@@ -0,0 +1,36 @@
+
+def accepts(*args):
+
+    def inner(func):
+
+        def validate(*argv):
+
+            if all(type(argv[i]) is args[i] for i in range(len(argv))):
+                return func(*argv)
+            else:
+                raise TypeError('Invalid argument types.')
+
+        return validate
+
+    return inner
+
+
+@accepts(str)
+def say_hello(name):
+    print(f'Hello, I am {name}!')
+
+
+@accepts(str, int)
+def deposit(name, money):
+    print(f'{name} sends ${money}!')
+
+
+def main():
+    deposit('Pesho', 50)
+    # deposit(50, 'Pesho')
+    say_hello('Gogi')
+    # say_hello(5)
+
+
+if __name__ == '__main__':
+    main()
